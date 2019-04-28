07cfb094743dc655ba570e95eae9618d7be9e3cd
Massimo Cappellano <Massimo.Cappe@gmail.com>
2019-04-28 13:27:09 +0200

    initial for tests

---
+++ src/porfoliodb/test/TestPortafoglioDB.java
@@ -0,0 +1,24 @@
+package porfoliodb.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestPortafoglioDB {
+
+	@Test
+	void testInsertOperazione() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetOperazioniOperationType() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetOperazioniOperationTypeDateDate() {
+		fail("Not yet implemented");
+	}
+
+}
